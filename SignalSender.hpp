14a3a2eb6e8bbb76e4b38c05c6442866590ca2fb
c0ffee <c0ffeeartc@.gmail.com>
2015-09-04 11:47:59 +0300

    remove improvement

--- SignalSender.hpp
+++ SignalSender.hpp
@@ -16,7 +16,7 @@ namespace sig
 //          signal() iterates over std::function(const Event&)
 //          remove() erases all matches to <classId: objId>
 //          objId requires
-//              Constructor(){static s_id=1; _id = s_id++;}
+//              Constructor(){static s_id=1; _id = s_id++;} // objId must be >0
 //              or similar for each Subscriber
 //              See for pitfalls:
 //                  http://stackoverflow.com/questions/14585385/best-practice-how-to-get-a-unique-identifier-for-the-object
@@ -81,7 +81,10 @@ public:
         }
     }
 
-
+    //
+    // TODO: consider objId = 0, instead of removing
+    //      and garbage collect later
+    //
     template<typename Subscriber>
     void
         remove
@@ -89,27 +92,10 @@ public:
     {
         ObjId removeId(type_id<Subscriber>(), objId);
 
-        auto it    = connections.begin();
         auto endIt = connections.end();
-
-        // collect
-        //
-        std::stack<typename Connections::iterator> st;
-        for(;;)
-        {
-            it = find_if (it, endIt, [&removeId](const ConnectionPair&c){ return c.first == removeId;});
-            if(it == endIt) break;
-            st.push(it);
-            it++;
-        }
-
-        // remove
-        //
-        while(!st.empty())
-        {
-            connections.erase(st.top());
-            st.pop();
-        }
+        auto eraseBegin = std::remove_if( connections.begin(), endIt
+                , [&removeId](const ConnectionPair&c){ return c.first == removeId; });
+        connections.erase(eraseBegin, endIt);
     }
 
 private:
@@ -121,6 +107,7 @@ private:
 //
 // global - use void/etc for SUBSCRIBER type
 // for objects use lambda with capture
+// do not use OBJ_ID == 0, it may become special value in future
 //
 #define CONNECT(EVENT, FUNC, SUBSCRIBER, OBJ_ID)\
     sig::SignalSender<EVENT>::getInstance().connect<SUBSCRIBER>\
