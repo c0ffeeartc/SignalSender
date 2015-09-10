0a1ab2464c28c1d5553c811c42b7b5ebc8e9c5bc
c0ffee <c0ffeeartc@.gmail.com>
2015-09-10 18:22:08 +0300

    merged into 1 file

--- SignalSender.hpp
+++ SignalSender.hpp
@@ -1,13 +1,27 @@
 #pragma once
-#include "type_id.hpp"
 #include <functional>
 #include <vector>
-#include <stack>
 #include <algorithm>
 
 
 namespace sig
 {
+
+
+//
+// http://codereview.stackexchange.com/questions/48594/unique-type-id-no-rtti
+//
+using typeid_t = void const*;
+
+template <typename T>
+static typeid_t
+    type_id() noexcept
+{
+    static char const type_id{};
+
+    return &type_id;
+}
+
 
 //
 // SignalSender<Event> singleton
